526fce480e4233967040f9f1e7fce64fbfc8327a
Jacqueline <airbornman1999@gmail.com>
2020-09-27 21:04:21 +0800

    Modified generator expression practicing script.

--- generator_practice.py
+++ generator_practice.py
@@ -5,13 +5,15 @@ import csv
 City = collections.namedtuple('City', ['city_name', 'country', 'population', 'latitude', 'longtitude'])
 
 with open('work_stack/worldcities.csv', 'r') as data_file:
-    csv_reader = csv.reader(data_file)
+    csv_reader = csv.DictReader(data_file)
 
-    city_generator = (City(*single_line) for single_line in csv_reader)
-    for single_city in city_generator:
-        if single_city.city_name == 'Zhengzhou':
-            print(single_city)
+    city_generator = (City(single_city['city_name'], single_city['country'], single_city['population'],
+                           float(single_city['latitude']), float(single_city['longtitude'])) for single_city in csv_reader)
 
-
-
-
+    city_counter = 0
+    for city_info in city_generator:
+        if city_info.country == 'United States' and int(city_info.population) > 1000000:
+            city_counter += 1
+            print(city_info.city_name, city_info.population)
+    
+    print('There are {} cities satisfied your criteria.'.format(city_counter))
