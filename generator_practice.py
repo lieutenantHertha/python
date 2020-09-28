c90fcfbda7cdf314ac7fb55f5ac27d97869ac075
Jacqueline <airbornman1999@gmail.com>
2020-09-28 18:49:50 +0800

    Modified generator expression and new added slice practition.

--- generator_practice.py
+++ generator_practice.py
@@ -7,13 +7,16 @@ City = collections.namedtuple('City', ['city_name', 'country', 'population', 'la
 with open('work_stack/worldcities.csv', 'r') as data_file:
     csv_reader = csv.DictReader(data_file)
 
-    city_generator = (City(single_city['city_name'], single_city['country'], single_city['population'],
-                           float(single_city['latitude']), float(single_city['longtitude'])) for single_city in csv_reader)
+    city_generator = (City(*single_city.values()) for single_city in csv_reader)
 
     city_counter = 0
     for city_info in city_generator:
-        if city_info.country == 'United States' and int(city_info.population) > 1000000:
-            city_counter += 1
-            print(city_info.city_name, city_info.population)
+        if city_info.population:
+            if float(city_info.longtitude) < 0.0 and int(city_info.population) > 5000000:
+                city_counter += 1
+                print('{:20}{:15}{:10.4f}{:10.4f}'.format(city_info.city_name, 
+                                                           city_info.population, 
+                                                           float(city_info.latitude), 
+                                                           float(city_info.longtitude)))
     
     print('There are {} cities satisfied your criteria.'.format(city_counter))
